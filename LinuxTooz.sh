26d856e0c3f4f0968e450622a92c64f0e39c8d50
FlamerMC <153970526+FlamerMC@users.noreply.github.com>
2024-12-24 20:56:47 -0500

    Add files via upload

---
+++ LinuxTooz.sh
@@ -0,0 +1,15 @@
+echo Updating Repos
+
+sleep 2
+
+echo INSTALLING GOOGLE CHROME
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update
+sudo apt install google-chrome-stable
+
+echo Setting chrome as default broswer 
+
+sleep 2
+
+xdg-settings set default-web-browser google-chrome.desktop
